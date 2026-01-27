aae5c48cf55534848bb1416979c8ffdcccb6c708
rtux754 <rozzaqm05@gmail.com>
2026-01-27 16:53:15 +0700

    aku menambahkan folder baru yang berisi project game latihan

---
+++ Echoes-of-January/Echoes-of-January.cs
@@ -0,0 +1,19 @@
+using System;
+namespace FirstTime;
+
+public static class Echoes {
+    public static void Jalankan() {
+        
+        string[] topik = [
+            "Puisi",
+            "Game",
+            "Coding",
+            "Buku"
+        ];
+
+        // int affinity = 0;
+
+        Console.ReadLine();
+
+    }
+}
